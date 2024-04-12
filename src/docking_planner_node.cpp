625790ba773bfa0d9c3e82cd1380fbdd57e9312c
Marcus-techtile <marcus@techtile.sg>
2024-04-12 08:10:36 +0700

    Update check goal reach: fail when goal_x < 0

--- src/docking_planner_node.cpp
+++ src/docking_planner_node.cpp
@@ -298,8 +298,8 @@ void DockingManager::checkGoalReach()
 
     if (check_inside_goal_range_)
     {
-        if (error_sq > distance_tolerance_) count_outside_goal_range_++;
-        if (count_outside_goal_range_ > 20)      // Prevent jumping in and out goal range
+        if (error_x < 0) count_outside_goal_range_++;
+        if (count_outside_goal_range_ > 10)      // Prevent jumping in and out goal range
         {
             count_outside_goal_range_ = 0;
             ROS_WARN("Failed!!! !");
