93cfad2d0de8da75d2d114ff025b0cc8868d4d88
Marcus-techtile <marcus@techtile.sg>
2024-06-03 09:51:03 +0700

    Add adaptive docking local planner

---
+++ include/adaptive_docking_local_planner.h
@@ -0,0 +1,46 @@
+#ifndef ADAPTIVE_DOCKING_LOCAL_PLANNER_H
+#define ADAPTIVE_DOCKING_LOCAL_PLANNER_H
+#include <ros/ros.h>
+#include "docking_planner.h"
+#include <mbf_msgs/ExePathResult.h>
+
+namespace techtile {
+class AdaptiveDockingLocalPlanner
+{
+private:
+    ros::NodeHandle nh_;
+
+    DockingManager docking_manager;
+
+public:
+    AdaptiveDockingLocalPlanner(ros::NodeHandle &nh);
+    ~AdaptiveDockingLocalPlanner();
+       
+    /**** MBF Interface ****/
+    // 
+    void initialize();
+    bool setPlan (const std_msgs::Header &header, 
+				const geometry_msgs::PoseStamped &starting_pose,
+				const geometry_msgs::PoseStamped &approaching_pose, 
+				const geometry_msgs::PoseStamped &docking_pose);
+    bool setPlan (const std_msgs::Header &header,
+                const geometry_msgs::PoseStamped &approaching_pose, 
+                const geometry_msgs::PoseStamped &docking_pose);
+    uint32_t ExecuteControlLoop(const geometry_msgs::PoseStamped &pose,
+                                    const geometry_msgs::TwistStamped &velocity, 
+                                    geometry_msgs::Twist &cmd_vel,
+                                    std::string &message);
+    bool IsGoalReached();
+
+    // Addition methods
+    void SetDockingTolerance (const double dx, const double dy, const double dyaw);
+    void SetApproachingTolerance (const double dx, const double dy, const double dyaw);
+    void SetMaxSpeed(const double max_linear_speed);
+
+
+
+
+
+};
+} //namespace
+#endif // ADAPTIVE_DOCKING_LOCAL_PLANNER_H
---
+++ src/adaptive_docking_local_planner.cpp
@@ -0,0 +1,48 @@
+#include "adaptive_docking_local_planner.h"
+
+using namespace techtile;
+
+AdaptiveDockingLocalPlanner::AdaptiveDockingLocalPlanner(ros::NodeHandle &nh) : nh_(nh), docking_manager(nh)
+{
+    initialize();
+}
+
+AdaptiveDockingLocalPlanner::~AdaptiveDockingLocalPlanner(){}
+
+void AdaptiveDockingLocalPlanner::initialize()
+{
+    docking_manager.initDocking();
+}
+
+bool AdaptiveDockingLocalPlanner::setPlan (const std_msgs::Header &header, 
+                const geometry_msgs::PoseStamped &starting_pose,
+                const geometry_msgs::PoseStamped &approaching_pose, 
+                const geometry_msgs::PoseStamped &docking_pose)
+{
+    return docking_manager.setupPoses(approaching_pose, docking_pose);
+}
+
+bool AdaptiveDockingLocalPlanner::setPlan (const std_msgs::Header &header,
+            const geometry_msgs::PoseStamped &approaching_pose, 
+            const geometry_msgs::PoseStamped &docking_pose)
+{
+    return docking_manager.setupPoses(approaching_pose, docking_pose);
+}
+
+uint32_t AdaptiveDockingLocalPlanner::ExecuteControlLoop(const geometry_msgs::PoseStamped &pose,
+                                const geometry_msgs::TwistStamped &velocity, 
+                                geometry_msgs::Twist &cmd_vel,
+                                std::string &message)
+{
+    docking_manager.dockingFSM();
+    if (docking_manager.docking_failed.data)
+        return mbf_msgs::ExePathResult::FAILURE;
+    return mbf_msgs::ExePathResult::SUCCESS;
+}
+
+bool AdaptiveDockingLocalPlanner::IsGoalReached()
+{
+    if (docking_manager.approaching_done.data && docking_manager.docking_done.data) 
+        return true;
+    else return false;
+}
